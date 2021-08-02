8ecff08805df383b6ba31d9760cf8d71ca116839
Eduard <jimlab@mail.ru>
2021-08-02 18:58:50 +0300

    add correct button selector

--- test_items.py
+++ test_items.py
@@ -5,4 +5,5 @@ def test_has_add_button(browser):
     print(browser)
     browser.get(link)
     time.sleep(3) # IMHO 30 is too much, but you free to change it
-    assert len(browser.find_elements_by_class_name('btn-add-to-basket1')) > 0, "No button element found!!!"
+    assert len(browser.find_elements_by_class_name('btn-add-to-basket')) > 0, "No button element found!!!"
+    # assert len(browser.find_elements_by_class_name('btn-add-to-basket1')) > 0, "No button element found!!!"
